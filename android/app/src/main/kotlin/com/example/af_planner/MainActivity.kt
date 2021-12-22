e7141d0ec5c07224f88accb2caaf9ba1347bd58d
Garrick <garrick.hilliard@gmail.com>
2021-12-22 16:55:44 -0700

    Created Project

---
+++ android/app/src/main/kotlin/com/example/af_planner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.af_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:af_planner/setup.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+  setup();
+  runApp(const MyApp());
+}
